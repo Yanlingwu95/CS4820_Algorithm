4ea738a91d122cec0ceffe411e12ea2e73965866
Yanling Wu <yanlingwu95@gmail.com>
2019-01-27 22:25:55 -0500

    the first time to create the file and add the algo book

---
+++ Homework/HW1/Framework.java
@@ -0,0 +1,121 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.util.*;
+
+public class Framework
+{
+	int n; // number of applicants (employers)
+
+	int APrefs[][]; // preference list of applicants (n*n)
+	int EPrefs[][]; // preference list of employers (n*n)
+
+	ArrayList<MatchedPair> MatchedPairsList; // your output should fill this arraylist which is empty at start
+
+	public class MatchedPair
+	{
+		int appl; // applicant's number
+		int empl; // employer's number
+
+		public MatchedPair(int Appl,int Empl)
+		{
+			appl=Appl;
+			empl=Empl;
+		}
+
+		public MatchedPair()
+		{
+		}
+	}
+
+	// reading the input
+	void input(String input_name)
+	{
+		File file = new File(input_name);
+		BufferedReader reader = null;
+
+		try
+		{
+			reader = new BufferedReader(new FileReader(file));
+
+			String text = reader.readLine();
+
+			String [] parts = text.split(" ");
+			n=Integer.parseInt(parts[0]);
+
+			APrefs=new int[n][n];
+			EPrefs=new int[n][n];
+
+			for (int i=0;i<n;i++)
+			{
+				text=reader.readLine();
+				String [] aList=text.split(" ");
+				for (int j=0;j<n;j++)
+				{
+					APrefs[i][j]=Integer.parseInt(aList[j]);
+				}
+			}
+
+			for (int i=0;i<n;i++)
+			{
+				text=reader.readLine();
+				String [] eList=text.split(" ");
+				for(int j=0;j<n;j++)
+				{
+					EPrefs[i][j]=Integer.parseInt(eList[j]);
+				}
+			}
+
+			reader.close();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	// writing the output
+	void output(String output_name)
+	{
+		try
+		{
+			PrintWriter writer = new PrintWriter(output_name, "UTF-8");
+
+			for(int i=0;i<MatchedPairsList.size();i++)
+			{
+				writer.println(MatchedPairsList.get(i).empl+" "+MatchedPairsList.get(i).appl);
+			}
+
+			writer.close();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	public Framework(String []Args)
+	{
+		input(Args[0]);
+
+		MatchedPairsList=new ArrayList<MatchedPair>(); // you should put the final stable matching in this array list
+
+		/* NOTE
+		 * if you want to declare that man x and woman y will get matched in the matching, you can
+		 * write a code similar to what follows:
+		 * MatchedPair pair=new MatchedPair(x,y);
+		 * MatchedPairsList.add(pair);
+		*/
+
+		//YOUR CODE STARTS HERE
+		//YOUR CODE ENDS HERE
+
+		output(Args[1]);
+	}
+
+	public static void main(String [] Args) // Strings in Args are the name of the input file followed by the name of the output file
+	{
+		new Framework(Args);
+	}
+}
