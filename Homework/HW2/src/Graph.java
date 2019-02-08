1fed1224bc4b280132cb8f9d63b9277954cfcceb
Yanling Wu <yanlingwu95@gmail.com>
2019-02-07 22:20:49 -0500

    Finish the MST coding

--- Homework/HW2/src/Graph.java
+++ Homework/HW2/src/Graph.java
@@ -4,15 +4,24 @@ public class Graph {
     //Construction function
     int N , M, P; // N-># of nodes, M-># of edges
     Edge edges[];//store all edges
+
     //Create a edge class to represent the graph edge
     class Edge implements Comparable<Edge> {
-        int stn, des, weight, no;
+        int stn, des, weight, id;
 
+        Edge(){};
+        Edge(int s, int d, int w, int no) {
+            stn = s;
+            des = d;
+            weight = w;
+            id = no;
+        }
         public int compareTo(Edge compareEdge) {
             return this.weight-compareEdge.weight;
         }
     };
 
+    //Create subset class
     class Subset {
         int parent, rank;
     };
@@ -55,12 +64,9 @@ public class Graph {
         }
     }
 
-    private void kruskalMST() {
+    private ArrayList<Edge> kruskalMST() {
         //Create the result's edge list and initial it
-        Edge[] res = new Edge[N];
-        for (int i = 0; i < N; i++) {
-            res[i] = new Edge();
-        }
+        ArrayList<Edge> mst = new ArrayList<>();
 
         //Sort all edges in ascending order
         Arrays.sort(edges);
@@ -76,95 +82,124 @@ public class Graph {
         //
         int i = 0, e = 0;
         while(e < N - 1) {
-            Edge next = new Edge();
-            next = edges[i++];
+            Edge next = edges[i++];
             int u = find(subsets, next.stn);
             int v = find(subsets, next.des);
             if ( u != v) {
-                res[e++] = next;
+                mst.add(next);
+                e++;
                 union(subsets, u, v);
             }
         }
-        for (i = 0; i < e; i++) {
-            System.out.println(res[i].no);
-            System.out.println(res[i].stn+" -- " +
-                    res[i].des+" == " + res[i].weight + "The edge number is  "
-                    + res[i].no);
+        return mst;
+    }
+
+    //***************************************//
+    public ArrayList<ArrayList<Edge>> createGraph(Edge[] edges) {
+        ArrayList<ArrayList<Edge>> G = new ArrayList<>();
+        int len = edges.length * 2;
+        //Initialize the arrayList
+        for (int i = 0; i < len; i++) {
+            G.add(new ArrayList<>());
         }
+        for(Edge e : edges) {
+            Edge one = new Edge(e.des, e.stn, e.weight, e.id);
+            G.get(e.stn).add(e);
+            G.get(e.des).add(one);
+        }
+        return G;
+    }
+
+    //
+    private ArrayList<Edge> prim(ArrayList<ArrayList<Edge>> G) {
+        if(G.isEmpty()) throw new NullPointerException("The Graph is empty");
+
+        ArrayList<Edge> mst = new ArrayList<>();
+
+        PriorityQueue<Edge> pq = new PriorityQueue<>((Object x, Object y) -> {
+            Edge first = (Edge) x;
+            Edge second = (Edge) y;
+            if(first.weight < second.weight) return -1;
+            else if(first.weight > second.weight) return 1;
+            else return 0;
+        });
+
+        for(Edge e : G.get(1)) {
+            pq.add(e);
+           // System.out.println("The id of e is " + e.id);
+        }
+
+        boolean[] marked = new boolean[G.size() + 1];
+        marked[1] = true;
+
+       // System.out.println("while begins!");
+
+        while(!pq.isEmpty()) {
+            Edge e = pq.poll(); //get the first edge and remove it
+            if(marked[e.stn] && marked[e.des])
+                continue;
+            marked[e.stn] = true;
+            for(Edge edge : G.get(e.des)) {
+                if(!marked[edge.des]) {
+                    pq.add(edge);
+                }
+            }
+            marked[e.des] = true;
+            mst.add(e);
+            //System.out.println("Should add the edge " + e.id);
+        }
+        return mst;
     }
 
     //main function
     public static void main(String[] Args) {
-        /*//define variables
+        //define variables
         Scanner scan = new Scanner(System.in);
         System.out.println("Please type the graph's information: ");
 
-        //Use nextLine to receive the data
-        //get the first line
-        String str = new String();
         //Get the first line to  initialize the graph
-        if (scan.hasNextLine()) {
-            str = scan.nextLine();
-        }
-        String[] str_list = str.split(" ");
-        int N = Integer.parseInt(str_list[0]);
-        int M = Integer.parseInt(str_list[1]);
-        int P = Integer.parseInt(str_list[2]);
-        Graph graph = new Graph(N, M);
+        int n = scan.nextInt();
+        int m = scan.nextInt();
+        int p = scan.nextInt();
+        Graph graph = new Graph(n, m);
 
-        //Get the edges information
-        for (int i = 0; i < M; i++) {
-            if (scan.hasNextLine()) {
-                str = scan.nextLine();
-            }
-            str_list = str.split(" ");
-            graph.edges[i].stn = Integer.parseInt(str_list[0]);
-            graph.edges[i].des = Integer.parseInt(str_list[1]);
-            graph.edges[i].weight = Integer.parseInt(str_list[2]);
-            graph.edges[i].no = i+1;
-        }
-
-        if (P == 0)
-            graph.kruskalMST();
-        if (P == 1)
-            graph.kruskalMST();*/
-
-        int N = 3; //# nodes
-        int M = 3;//# edges
-        Graph graph = new Graph(N, M);
+        /*//Get the edges information
+        for (int i = 0; i < m; i++) {
+            graph.edges[i].stn = scan.nextInt();
+            graph.edges[i].des = scan.nextInt();
+            graph.edges[i].weight = scan.nextInt();
+            graph.edges[i].id = i+1;
+        }*/
 
         graph.edges[0].stn = 1;
         graph.edges[0].des = 2;
         graph.edges[0].weight = 100;
-        graph.edges[0].no = 1;
+        graph.edges[0].id = 1;
 
         // add edge 0-2
         graph.edges[1].stn = 3;
         graph.edges[1].des = 2;
         graph.edges[1].weight = 200;
-        graph.edges[1].no = 2;
+        graph.edges[1].id = 2;
 
         // add edge 0-3
         graph.edges[2].stn = 1;
         graph.edges[2].des = 3;
         graph.edges[2].weight = 150;
-        graph.edges[2].no = 3;
+        graph.edges[2].id = 3;
 
-        graph.kruskalMST();
+        ArrayList<Edge> mst = new ArrayList<>();
+
+        if (p == 0){
+            mst = graph.kruskalMST();
+        }
+        if (p == 1){
+            ArrayList<ArrayList<Edge>> G = graph.createGraph(graph.edges);
+            mst = graph.prim(G);
+        }
+
+        for (int i = 0; i < mst.size(); i++) {
+            System.out.println(mst.get(i).id);
+        }
     }
-
 }
-
-
-/*
-// add edge 1-3
-        graph.edges[3].stn = 1;
-                graph.edges[3].des = 3;
-                graph.edges[3].weight = 15;
-                graph.edges[3].no = 4;
-
-                // add edge 2-3
-                graph.edges[4].stn = 2;
-                graph.edges[4].des = 3;
-                graph.edges[4].weight = 4;
-                graph.edges[4].no = 5;*/
